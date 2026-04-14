a20bd0f469c71e85d23709e9c59bc90d56905dd8
Mauli1022 <palkemauli1022@gmail.com>
2026-04-14 15:49:30 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import express from "express"
+import dotenv from "dotenv"
+
+const app = express();
+dotenv.config({
+    path : "./.env"
+})
+
+
+const PORT = process.env.PORT ?? 8000
+
+app.get("/",(req,res)=>{
+    res.send(
+        `
+        <h1>This is My Cicd Demo Project</h1>
+        `
+    )
+})
+
+
+
+app.listen(PORT,()=>{
+    console.log("Server Is Running on http://localhost:"+PORT);    
+})
